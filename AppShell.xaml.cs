7a4b9caf0b84a0adbc2d393b9e38fc5f882a618e
lorenz | onionware <lorenzotto060@gmail.com>
2024-02-04 14:54:36 +0100

    1

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GymHelper;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GymHelper;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
